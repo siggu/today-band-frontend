c4ecd8fb31489d8358909a212b8f432c8929dedb
siggu <m0_w_0m@naver.com>
2024-12-04 16:38:48 +0900

    feat: 헤더 페이지 이름, drawer 추가

---
+++ src/components/Header.tsx
@@ -0,0 +1,56 @@
+import { Button } from './ui/button';
+import {
+  DrawerBackdrop,
+  DrawerBody,
+  DrawerCloseTrigger,
+  DrawerContent,
+  DrawerHeader,
+  DrawerRoot,
+  DrawerTrigger,
+} from './ui/drawer';
+import { AccordionItem, AccordionItemContent, AccordionItemTrigger, AccordionRoot } from './ui/accordion';
+import { HStack, Box, Text } from '@chakra-ui/react'; // Chakra UI 요소
+import { useState } from 'react';
+
+export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  const bands = [
+    { value: '1', title: '가', text: '가밴드' },
+    { value: '2', title: '나', text: '나밴드' },
+    { value: '3', title: '다', text: '다밴드' },
+  ];
+
+  return (
+    <HStack justifyContent='space-between' padding='16px' alignItems='center'>
+      <Box flex='1' textAlign='center'>
+        <Text fontSize='30px' fontWeight='bold'>
+          오늘의 밴드
+        </Text>
+      </Box>
+
+      <Box justifyContent={'center'} alignItems={'center'}>
+        <DrawerRoot size={'md'} open={open} onOpenChange={(e: any) => setOpen(e.open)}>
+          <DrawerBackdrop />
+          <DrawerTrigger>
+            <Button rounded={5}>밴드 목록</Button>
+          </DrawerTrigger>
+          <DrawerContent rounded='5px' offset={4}>
+            <DrawerHeader fontSize={'18px'}>밴드 목록</DrawerHeader>
+            <DrawerBody>
+              <AccordionRoot rounded='5px' variant={'enclosed'} size={'lg'} multiple defaultValue={['1']}>
+                {bands.map((band, index) => (
+                  <AccordionItem key={index} value={band.value}>
+                    <AccordionItemTrigger>{band.title}</AccordionItemTrigger>
+                    <AccordionItemContent>{band.text}</AccordionItemContent>
+                  </AccordionItem>
+                ))}
+              </AccordionRoot>
+            </DrawerBody>
+            <DrawerCloseTrigger />
+          </DrawerContent>
+        </DrawerRoot>
+      </Box>
+    </HStack>
+  );
+}
