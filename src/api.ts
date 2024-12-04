905a0b7784a5f70e8960b1800f8a00a9fa023594
siggu <m0_w_0m@naver.com>
2024-12-04 21:07:56 +0900

    refactor: axios 적용

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
-const BASE_URL = 'http://127.0.0.1:8000/api/v1';
+import axios from 'axios';
 
-export async function getBands() {
-  const response = await fetch(`${BASE_URL}/bands/`);
-  const json = await response.json();
-  return json;
-}
+const instance = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api/v1',
+});
+
+export const getBands = () => instance.get('bands/').then((response) => response.data);
