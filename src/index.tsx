536d15f33b4860a000e1a07159d8dc58b953587b
siggu <m0_w_0m@naver.com>
2024-12-04 15:43:44 +0900

    fix: ChakraProvider 업데이트

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider, createSystem, defineConfig } from '@chakra-ui/react';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-
-const config = defineConfig({
-  theme: {
-    tokens: {
-      colors: {},
-    },
-  },
-});
-
-const system = createSystem(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ChakraProvider value={system}>
+    <ChakraProvider value={defaultSystem}>
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
