606244c290826cf6e62c3df2aac14664dcd06a48
siggu <m0_w_0m@naver.com>
2024-12-14 15:16:45 +0900

    fix: header 로고 고정 오류 해결

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -53,15 +53,17 @@ export default function Header() {
   ];
 
   return (
-    <HStack justifyContent='space-between' padding='30px' alignItems='center'>
-      <Button size={'sm'} onClick={toggleColorMode} rounded={5}>
-        <MdDarkMode />
-      </Button>
-      <Button size={'sm'} onClick={() => refreshPage(`/comments`)}>
-        댓글
-      </Button>
+    <Box position='relative' padding='30px'>
+      <Box position='absolute' left='0' top='50%' transform='translateY(-50%)'>
+        <Button mx={2} size={'sm'} onClick={toggleColorMode} rounded={5}>
+          <MdDarkMode />
+        </Button>
+        <Button size={'sm'} onClick={() => refreshPage(`/comments`)}>
+          댓글
+        </Button>
+      </Box>
 
-      <Box flex='1' textAlign='center'>
+      <Box textAlign='center'>
         <Box onClick={() => refreshPage(`/`)}>
           <Text fontSize='30px' fontWeight='bold'>
             오늘의 밴드
@@ -69,52 +71,55 @@ export default function Header() {
         </Box>
       </Box>
 
-      {!userLoading ? (
-        !isLoggedIn ? (
-          <>
-            <LoginDialog />
-          </>
-        ) : (
-          <LogOutDialog />
-        )
-      ) : null}
-
-      <Box justifyContent={'center'} alignItems={'center'}>
-        <DrawerRoot size={'md'} open={open} onOpenChange={(e: any) => setOpen(e.open)}>
-          <DrawerBackdrop />
-          <DrawerTrigger>
-            <Button rounded={5}>밴드 목록</Button>
-          </DrawerTrigger>
-          <DrawerContent rounded='5px' offset={4}>
-            <DrawerHeader fontSize={'18px'}>밴드 목록</DrawerHeader>
-            <DrawerBody>
-              <AccordionRoot rounded='5px' variant={'enclosed'} size={'lg'} multiple defaultValue={[]}>
-                {classifications.map((cls) => (
-                  <AccordionItem key={cls.code} value={cls.code}>
-                    <AccordionItemTrigger>{cls.title}</AccordionItemTrigger>
-                    <AccordionItemContent>
-                      {isLoading ? (
-                        <Text>Loading...</Text>
-                      ) : (
-                        data
-                          .filter((band: any) => band.classification === cls.code)
-                          .map((band: any) => (
-                            <Box key={band.id} marginBottom={4}>
-                              <Box onClick={() => refreshPage(`/bands/${band.id}`)}>
-                                <Text fontWeight={'bold'}>{band.name}</Text>
+      <HStack position='absolute' right='0' top='50%' transform='translateY(-50%)'>
+        <Box>
+          {!userLoading ? (
+            !isLoggedIn ? (
+              <>
+                <LoginDialog />
+              </>
+            ) : (
+              <LogOutDialog />
+            )
+          ) : null}
+        </Box>
+        <Box>
+          <DrawerRoot size={'md'} open={open} onOpenChange={(e: any) => setOpen(e.open)}>
+            <DrawerBackdrop />
+            <DrawerTrigger>
+              <Button rounded={5}>밴드 목록</Button>
+            </DrawerTrigger>
+            <DrawerContent rounded='5px'>
+              <DrawerHeader fontSize={'18px'}>밴드 목록</DrawerHeader>
+              <DrawerBody>
+                <AccordionRoot rounded='5px' variant={'enclosed'} size={'lg'} multiple defaultValue={[]}>
+                  {classifications.map((cls) => (
+                    <AccordionItem key={cls.code} value={cls.code}>
+                      <AccordionItemTrigger>{cls.title}</AccordionItemTrigger>
+                      <AccordionItemContent>
+                        {isLoading ? (
+                          <Text>Loading...</Text>
+                        ) : (
+                          data
+                            .filter((band: any) => band.classification === cls.code)
+                            .map((band: any) => (
+                              <Box key={band.id} marginBottom={4}>
+                                <Box onClick={() => refreshPage(`/bands/${band.id}`)}>
+                                  <Text fontWeight={'bold'}>{band.name}</Text>
+                                </Box>
                               </Box>
-                            </Box>
-                          ))
-                      )}
-                    </AccordionItemContent>
-                  </AccordionItem>
-                ))}
-              </AccordionRoot>
-            </DrawerBody>
-            <DrawerCloseTrigger />
-          </DrawerContent>
-        </DrawerRoot>
-      </Box>
-    </HStack>
+                            ))
+                        )}
+                      </AccordionItemContent>
+                    </AccordionItem>
+                  ))}
+                </AccordionRoot>
+              </DrawerBody>
+              <DrawerCloseTrigger />
+            </DrawerContent>
+          </DrawerRoot>
+        </Box>
+      </HStack>
+    </Box>
   );
 }
