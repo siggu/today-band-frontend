518780a280a13becc79e4e11408f599d77a242e5
siggu <m0_w_0m@naver.com>
2024-12-27 22:13:13 +0900

    fix: backend 주소 변경

--- src/api.ts
+++ src/api.ts
@@ -5,7 +5,7 @@ import { IComment, IUsernameLoginVariables } from './types.d';
 
 const instance = axios.create({
   baseURL:
-    process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/v1/' : 'https://backend.today-band.site/api/v1',
+    process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/v1/' : 'https://today-band.onrender.com/api/v1',
   withCredentials: true,
 });
 
